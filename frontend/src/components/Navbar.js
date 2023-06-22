fe200b6d6bc551c27e72c9a32006a0e0dc8941d6
f1x3d49 <davidnicoara28@gmail.com>
2023-06-22 21:19:07 +0300

    working on the navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,14 +3,24 @@ import { ReactComponent as Logo } from "../images/logo.svg";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between w-full py-12 px-24">
+    <nav className="flex items-center justify-between w-full py-16 px-36">
       <Logo />
-      <ul>
-        <li>About</li>
-        <li>Careers</li>
-        <li>Events</li>
-        <li>Products</li>
-        <li>Support</li>
+      <ul className="hidden md:flex text-lg font-alata text-white font-normal">
+        <li className="p-4 relative hover:cursor-pointer nav">
+          <a href="/">About</a>
+        </li>
+        <li className="p-4 relative hover:cursor-pointer nav">
+          <a href="/">Careers</a>
+        </li>
+        <li className="p-4 relative hover:cursor-pointer nav">
+          <a href="/">Events</a>
+        </li>
+        <li className="p-4 relative hover:cursor-pointer nav">
+          <a href="/">Products</a>
+        </li>
+        <li className="p-4 relative hover:cursor-pointer nav">
+          <a href="/">Support</a>
+        </li>
       </ul>
     </nav>
   );
