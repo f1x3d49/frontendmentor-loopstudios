e385d17eda21349d37cd12054cfd949eee791f39
f1x3d49 <davidnicoara28@gmail.com>
2023-06-24 20:45:16 +0300

    finished the project

--- frontend/src/sections/AboutSection.js
+++ frontend/src/sections/AboutSection.js
@@ -7,17 +7,35 @@ import { ReactComponent as Pinterest } from "../images/icon-pinterest.svg";
 
 const AboutSection = () => {
   return (
-    <section className="w-full flex flex-col items-center gap-6 h-96 bg-black mt-16 px-8 py-4">
-      <Logo />
-      <ul className="flex flex-col md:flex-row gap-2">
-        <li>About</li>
-        <li>Careers</li>
-        <li>Events</li>
-        <li>Products</li>
-        <li>Support</li>
-      </ul>
-      <div className="flex flex-col gap-1">
-        <ul className="flex gap-1"></ul>
+    <section className="w-full flex flex-col md:flex-row md:justify-between items-center gap-8 md:h-48 h-[420px] bg-black mt-16 md:px-24 px-8 py-10">
+      <div className="flex flex-col gap-8 md:gap-10">
+        <Logo className="scale-90" />
+        <ul className="flex flex-col md:flex-row gap-4 font-alata font-light text-white text-center">
+          <li className="relative nav hover:cursor-pointer">About</li>
+          <li className="relative nav hover:cursor-pointer">Careers</li>
+          <li className="relative nav hover:cursor-pointer">Events</li>
+          <li className="relative nav hover:cursor-pointer">Products</li>
+          <li className="relative nav hover:cursor-pointer">Support</li>
+        </ul>
+      </div>
+      <div className="flex flex-col gap-2 items-center md:gap-10 md:items-end">
+        <ul className="flex gap-4">
+          <li>
+            <Facebook className="relative nav hover:cursor-pointer" />
+          </li>
+          <li>
+            <Twitter className="relative nav hover:cursor-pointer" />
+          </li>
+          <li>
+            <Pinterest className="relative nav hover:cursor-pointer" />
+          </li>
+          <li>
+            <Instagram className="relative nav hover:cursor-pointer" />
+          </li>
+        </ul>
+        <p className="text-vdgray text-sm font-alata">
+          &copy; 2021 Loopstudios. All rights reserved.
+        </p>
       </div>
     </section>
   );
